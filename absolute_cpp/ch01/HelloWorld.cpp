7d8a7884ad1467c2d334a115f41ffe06bb34cb39
reentrant <sd.engineer@gmail.com>
2025-12-15 07:25:50 -0600

    Absolute CPP - chapter 01

---
+++ absolute_cpp/ch01/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // pre-processor directive
+
+int main( )
+{
+
+	std::cout << "Hello World!\n"; 
+//    return 0;
+}
---
+++ absolute_cpp/ch01/ud210_calc_area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+     int main()
+     {
+          float width = 4.5;
+          float height = 5.5;
+
+          float area = width * height;
+          std::cout << "area = " << area << " m^2"<<std::endl;
+          return 0;
+     }
